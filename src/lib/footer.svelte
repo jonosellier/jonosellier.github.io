00ea96b6bb90ab4358a8f06a0cfc60b2682a6b96
Jonathan Sellier <jonosellier@gmail.com>
2024-06-08 10:07:10 -0400

    Add footer links

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -4,9 +4,24 @@
 		<div class="w-sm">
 			<h2 class="text-xl font-bold">Social</h2>
 			<ul>
-				<li>Linkedin</li>
-				<li>GitHub</li>
-				<li>Dev.to</li>
+				<li>
+					<a
+						class="base"
+						href="//linkedin.com/in/jonosellier"
+						target="_blank"
+						rel="noopener noreferrer">Linkedin</a
+					>
+				</li>
+				<li>
+					<a class="base" href="//github.com/jonosellier" target="_blank" rel="noopener noreferrer"
+						>GitHub</a
+					>
+				</li>
+				<li>
+					<a class="base" href="//dev.to/jonosellier" target="_blank" rel="noopener noreferrer"
+						>DEV Community</a
+					>
+				</li>
 			</ul>
 		</div>
 	</div>
